207e5799e42de24196d8b5250f43b3ba3c45db8f
nascal3 <nascal3@gmail.com>
2017-05-18 01:41:52 +0300

    make index post from database

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,16 +5,23 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
+
+                @foreach($posts as $post)
+
                 <article class="post-item">
+
+                    @if($post->image_url)
                     <div class="post-item-image">
                         <a href="post.html">
-                            <img src="img/Post_Image_1.jpg" alt="">
+                            <img src="{{$post->image_url}}" alt="">
                         </a>
                     </div>
+                    @endif
+
                     <div class="post-item-body">
                         <div class="padding-10">
-                            <h2><a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptas, blanditiis impedit repellat harum, eaque saepe aspernatur quo magnam obcaecati dolor! Deleniti quod repellendus non iste architecto, voluptate excepturi velit.</p>
+                            <h2><a href="post.html">{{$post->title}}</a></h2>
+                            <p>{{$post->excerpt}}</p>
                         </div>
 
                         <div class="post-meta padding-10 clearfix">
@@ -32,60 +39,8 @@
                         </div>
                     </div>
                 </article>
-                <article class="post-item">
-                    <div class="post-item-image">
-                        <a href="post.html">
-                            <img src="img/Post_Image_2.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="post-item-body">
-                        <div class="padding-10">
-                            <h2><a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptas, blanditiis impedit repellat harum, eaque saepe aspernatur quo magnam obcaecati dolor! Deleniti quod repellendus non iste architecto, voluptate excepturi velit.</p>
-                        </div>
 
-                        <div class="post-meta padding-10 clearfix">
-                            <div class="pull-left">
-                                <ul class="post-meta-group">
-                                    <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
-                                    <li><i class="fa fa-clock-o"></i><time> February 12, 2016</time></li>
-                                    <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
-                                    <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
-                                </ul>
-                            </div>
-                            <div class="pull-right">
-                                <a href="post.html">Continue Reading &raquo;</a>
-                            </div>
-                        </div>
-                    </div>
-                </article>
-                <article class="post-item">
-                    <div class="post-item-image">
-                        <a href="post.html">
-                            <img src="img/Post_Image_3.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="post-item-body">
-                        <div class="padding-10">
-                            <h2><a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit, consectetur adipisicing elit</a></h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptas, blanditiis impedit repellat harum, eaque saepe aspernatur quo magnam obcaecati dolor! Deleniti quod repellendus non iste architecto, voluptate excepturi velit.</p>
-                        </div>
-
-                        <div class="post-meta padding-10 clearfix">
-                            <div class="pull-left">
-                                <ul class="post-meta-group">
-                                    <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
-                                    <li><i class="fa fa-clock-o"></i><time> February 12, 2016</time></li>
-                                    <li><i class="fa fa-tags"></i><a href="#"> Vue Js</a>, <a href="#"> Laravel</a></li>
-                                    <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
-                                </ul>
-                            </div>
-                            <div class="pull-right">
-                                <a href="post.html">Continue Reading &raquo;</a>
-                            </div>
-                        </div>
-                    </div>
-                </article>
+                @endforeach
 
                 <nav>
                   <ul class="pager">
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('blog.index');
-});
+Route::get('/', ['uses' => 'BlogController@index']);
 
 Route::get('/blog/show', function () {
     return view('blog.show');
