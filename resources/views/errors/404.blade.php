79c97b5a4aa034549d1962569df19daeab8e4aa1
nascal3 <nascal3@gmail.com>
2017-05-23 02:31:22 +0300

    created error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main)
+
+    @section('contet')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Page Not Found</h2>
+                    <p>Sorry the page you are looking for couldn't be found</p>
+                </div>
+            </div>
+        </div>
+
+
+    @endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main)
+
+    @section('contet')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>An Error Occured</h2>
+                    <p>Whoops sorry something went wrong</p>
+                </div>
+            </div>
+        </div>
+
+
+    @endsection
